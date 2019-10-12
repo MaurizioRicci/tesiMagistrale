6f232a0246652724ae21a68edb6e9a79394c2125
mauricci <MaurizioRicci@users.noreply.github.com>
2019-10-12 11:17:00 +0200

    fix nome

--- src/router/index.js
+++ src/router/index.js
@@ -14,7 +14,7 @@ export default new Router({
       path: '/',
       name: 'login',
       component: {
-        name: 'LoginPagee',
+        name: 'PageLogin',
         components: { MasterPage, LoginPage },
         template: '<MasterPage><LoginPage/></MasterPage>'
       }
