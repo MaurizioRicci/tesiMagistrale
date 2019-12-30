d34a34ef025b13a56475288b6fccf50e921b4468
mauricci <MaurizioRicci@users.noreply.github.com>
2019-12-30 13:56:13 +0100

    upgrade to bootstrap-vue new version & menu UI style

--- src/components/ui/Menu.vue
+++ src/components/ui/Menu.vue
@@ -4,46 +4,39 @@
       <b-navbar-brand to="/home">Home</b-navbar-brand>
       <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
       <b-collapse id="nav-collapse" is-nav>
+      <b-navbar-nav class="w-100" align="center">
+        <b-nav-item-dropdown text="Beni" class="d-inline-block">
+        <b-dropdown-item to="/bene/aggiungi">Aggiungi un bene</b-dropdown-item>
+        <b-dropdown-item to="/bene/dettagli_bene">Visualizza un bene</b-dropdown-item>
+        <b-dropdown-item to="/bene/ricerca">Ricerca/Modifica beni</b-dropdown-item>
+        </b-nav-item-dropdown>
+      </b-navbar-nav>
 
-        <b-navbar-nav class="w-100">
-          <b-container fluid>
-          <b-row>
-            <b-col>
-            <b-nav-item-dropdown text="Beni" class="d-inline-block">
-              <b-dropdown-item to="/bene/aggiungi">Aggiungi un bene</b-dropdown-item>
-              <b-dropdown-item to="/bene/dettagli_bene">Visualizza un bene</b-dropdown-item>
-              <b-dropdown-item to="/bene/ricerca">Ricerca/Modifica beni</b-dropdown-item>
-            </b-nav-item-dropdown>
-            </b-col>
+      <b-navbar-nav class="w-100" align="center">
+        <b-nav-item-dropdown text="Funzioni" class="d-inline-block">
+          <b-dropdown-item to="/funzione/aggiungi">Aggiungi una funzione</b-dropdown-item>
+          <b-dropdown-item to="/funzione/ricerca">Ricerca/Modifica funzioni</b-dropdown-item>
+        </b-nav-item-dropdown>
+      </b-navbar-nav>
 
-            <b-col>
-            <b-nav-item-dropdown text="Funzioni" class="d-inline-block">
-              <b-dropdown-item to="/funzione/aggiungi">Aggiungi una funzione</b-dropdown-item>
-              <b-dropdown-item to="/funzione/ricerca">Ricerca/Modifica funzioni</b-dropdown-item>
-            </b-nav-item-dropdown>
-            </b-col>
+      <b-navbar-nav class="w-100" align="center">
+        <b-nav-item-dropdown text="Il mio lavoro" class="d-inline-block">
+          <b-dropdown-item to="/bene/mio_lavoro">I miei beni</b-dropdown-item>
+          <b-dropdown-item to="/funzione/mio_lavoro">Le mie funzioni</b-dropdown-item>
+      </b-nav-item-dropdown>
+      </b-navbar-nav>
 
-            <b-col>
-            <b-nav-item-dropdown text="Il mio lavoro" class="d-inline-block">
-              <b-dropdown-item to="/bene/mio_lavoro">I miei beni</b-dropdown-item>
-              <b-dropdown-item to="/funzione/mio_lavoro">Le mie funzioni</b-dropdown-item>
-            </b-nav-item-dropdown>
-            </b-col>
-
-            <b-col >
-              <b-navbar-nav>
-                <b-nav-item to="/">
-                  <span style="color:white;" :class="{'text-warning': !currUsername}"
-                  v-b-tooltip.hover.bottom="loginTooltip">
-                  User: {{currUsername || '???'}}
-                  </span>
-                </b-nav-item>
-              </b-navbar-nav>
-            </b-col>
-          </b-row>
-          </b-container>
-
+      <b-navbar-nav class="w-100" align="center">
+        <b-navbar-nav>
+          <b-nav-item to="/">
+            <span style="color:white;" :class="{'text-warning': !currUsername}"
+              v-b-tooltip.hover.bottom="loginTooltip">
+                User: {{currUsername || '???'}}
+            </span>
+          </b-nav-item>
         </b-navbar-nav>
+      </b-navbar-nav>
+
       </b-collapse>
     </b-navbar>
   </b-container>
@@ -67,4 +60,7 @@ export default {
 <style>
 .myMenu .nav-link { color: white !important; }
 .myMenu .nav-item:hover { background-color: #114e61; border-radius: 5px; }
+.dropdown-item:hover, .dropdown-item:focus {
+  background-color: #45b0d047;
+}
 </style>
