73ac3b2116be8ce95dcd4be8382f836efc421122
mauricci <MaurizioRicci@users.noreply.github.com>
2020-02-20 22:07:38 +0100

    caching dei dizionari

--- src/assets/js/loadDict.js
+++ src/assets/js/loadDict.js
@@ -1,13 +1,22 @@
 import axios from 'axios'
+let cached = {}
+
+async function getOrCache (url) {
+  if (!cached[url]) {
+    let resp = await axios.get(url)
+    cached[url] = resp
+  }
+  return Promise.resolve(cached[url])
+}
 
 // carica le voci dei dizionari controllati
-let loadDescr = (T) => axios.get(T.$store.getters.getDizionarioURL + '?' + 'dict_name=descrizione')
-let loadEsistenza = (T) => axios.get(T.$store.getters.getDizionarioURL + '?' + 'dict_name=esistenza')
-let loadFunc = (T) => axios.get(T.$store.getters.getDizionarioURL + '?' + 'dict_name=funzione')
-let loadMacroEpocaCar = (T) => axios.get(T.$store.getters.getDizionarioURL + '?' + 'dict_name=macroEpocaCar')
-let loadMacroEpocaOrig = (T) => axios.get(T.$store.getters.getDizionarioURL + '?' + 'dict_name=macroEpocaOrig')
-let loadRuolo = (T) => axios.get(T.$store.getters.getDizionarioURL + '?' + 'dict_name=ruolo')
-let loadTipoData = (T) => axios.get(T.$store.getters.getDizionarioURL + '?' + 'dict_name=tipoData')
+let loadDescr = (T) => getOrCache(T.$store.getters.getDizionarioURL + '?' + 'dict_name=descrizione')
+let loadEsistenza = (T) => getOrCache(T.$store.getters.getDizionarioURL + '?' + 'dict_name=esistenza')
+let loadFunc = (T) => getOrCache(T.$store.getters.getDizionarioURL + '?' + 'dict_name=funzione')
+let loadMacroEpocaCar = (T) => getOrCache(T.$store.getters.getDizionarioURL + '?' + 'dict_name=macroEpocaCar')
+let loadMacroEpocaOrig = (T) => getOrCache(T.$store.getters.getDizionarioURL + '?' + 'dict_name=macroEpocaOrig')
+let loadRuolo = (T) => getOrCache(T.$store.getters.getDizionarioURL + '?' + 'dict_name=ruolo')
+let loadTipoData = (T) => getOrCache(T.$store.getters.getDizionarioURL + '?' + 'dict_name=tipoData')
 
 export {
   loadDescr,
