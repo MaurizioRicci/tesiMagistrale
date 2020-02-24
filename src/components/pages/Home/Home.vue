756f350ae855fe25716dae797f459c68c15142ba
mauricci <MaurizioRicci@users.noreply.github.com>
2020-02-24 16:45:35 +0100

    home page restyling

--- src/components/pages/Home/Home.vue
+++ src/components/pages/Home/Home.vue
@@ -1,62 +1,68 @@
 <template>
   <b-container fluid>
-      <b-row>
-        <b-col cols="12" v-if="!noMenu"><Menu/></b-col>
-        <b-col>
-          <h2 v-if="!noTitle">{{title || 'Home page'}}</h2>
-        </b-col>
-      </b-row>
-      <b-row class="mt-3" align-h="center">
-          <h5 class="col-12">Benvenuto
-            <b-badge variant="info">{{$store.getters.getUserData.username}}</b-badge>
-          </h5>
-          <!-- Breve descrizione -->
-          <b-col class="text-left mt-1" cols="12">
-            <b-row align-h="center">
-              <b-col cols="12" md="10" lg="9">
-                <p>
-                  Questa applicazione permette di acquisire dati per il sistema di Historical GIS. I dati sono formati da <b-badge>Beni</b-badge>
-                  (identificano un luogo sul territorio dove esiste o esisteva una entità geografica) e <b-badge>Funzioni</b-badge>
-                  (descrivono le relazione tra beni; es: il bene 1 paga un tributo al bene 2).
-                  La fase di acquisizione consiste nell'individuare Beni e Funzioni che legano insieme tali Beni. Prima di acquisire occorre andare sulle apposite pagine
-                    <b-breadcrumb>
-                      <b-breadcrumb-item active>Beni</b-breadcrumb-item>
-                      <b-breadcrumb-item to="/bene/ricerca">Ricerca Beni</b-breadcrumb-item>
-                    </b-breadcrumb>
-                    e
-                    <b-breadcrumb>
-                      <b-breadcrumb-item active>Funzioni</b-breadcrumb-item>
-                      <b-breadcrumb-item to="/funzione/ricerca">Ricerca Funzioni</b-breadcrumb-item>
-                    </b-breadcrumb>
-                  per verificare che il lavoro non sia già stato fatto da altri.
-                </p>
-                <p>
-                  La barra principale in alto, il <b-badge>Menu</b-badge> ha le seguenti aree sensibili:
-                  <ul>
-                  <li>Home: va alla home del progetto</li>
-                  <li>Beni: apre un menu per analizzare i Beni presenti in archivio, per modificarli o aggiungerne altri</li>
-                  <li>Funzioni: apre un menu per analizzare le Funzioni presenti in archivio, per modificarle o aggiungerne altre</li>
-                  <li>Il mio lavoro: apre un menu per analizzare le i Beni o le Funzioni che sono state create dall'utente o che sono in attesa di revisione</li>
-                  </ul>
-                </p>
-              </b-col>
-            </b-row>
-          </b-col>
+    <b-row>
+      <b-col cols="12" v-if="!noMenu">
+        <Menu/>
+      </b-col>
+      <b-col>
+        <h2 v-if="!noTitle">{{title || 'Home page'}}</h2>
+      </b-col>
+    </b-row>
+    <b-row class="mt-3" align-h="center">
+      <h5 class="col-12">
+        Benvenuto
+        <b-badge variant="info">{{$store.getters.getUserData.username}}</b-badge>
+      </h5>
+      <!-- Breve descrizione + Collegamenti rapidi -->
+      <b-col class="text-left mt-1" cols="12" md="10" lg="6">
+        <p>
+          Questa applicazione permette di acquisire dati per il sistema di Historical GIS. I dati sono formati da
+          <b-badge>Beni</b-badge>
+          (identificano un luogo sul territorio dove esiste o esisteva una entità geografica) e
+          <b-badge>Funzioni</b-badge>
+          (descrivono le relazione tra beni; es: il bene 1 paga un tributo al bene 2).
+          La fase di acquisizione consiste nell'individuare Beni e Funzioni che legano insieme tali Beni. Prima di acquisire occorre andare sulle apposite pagine
+          <b-breadcrumb>
+            <b-breadcrumb-item active>Beni</b-breadcrumb-item>
+            <b-breadcrumb-item to="/bene/ricerca">Ricerca Beni</b-breadcrumb-item>
+          </b-breadcrumb>
+          e
+          <b-breadcrumb>
+            <b-breadcrumb-item active>Funzioni</b-breadcrumb-item>
+            <b-breadcrumb-item to="/funzione/ricerca">Ricerca Funzioni</b-breadcrumb-item>
+          </b-breadcrumb>
+          per verificare che il lavoro non sia già stato fatto da altri.
+        </p>
+        <p>
+          La barra principale in alto, il
+          <b-badge>Menu</b-badge>
+          ha le seguenti aree sensibili:
+        <ul>
+          <li>Home: va alla home del progetto</li>
+          <li>Beni: apre un menu per analizzare i Beni presenti in archivio, per modificarli o aggiungerne altri</li>
+          <li>Funzioni: apre un menu per analizzare le Funzioni presenti in archivio, per modificarle o aggiungerne altre</li>
+          <li>Il mio lavoro: apre un menu per analizzare le i Beni o le Funzioni che sono state create dall'utente o che sono in attesa di revisione</li>
+        </ul>
+        </p>
+        <b-row align-h="center" no-gutters style="margin-top: -1rem">
           <!-- Collegamenti rapidi -->
-          <b-col class="text-left mt-1" cols="12" md="6" lg="3">
+          <b-col class="text-left mt-0" cols="auto">
             <b-card header="Collegamenti rapidi" no-body bg-variant="info" text-variant="white">
               <b-list-group flush>
                 <b-list-group-item>
                   <b-link to="/bene/aggiungi">
-                  Aggiungi un bene</b-link>
-                  </b-list-group-item>
+                    Aggiungi un bene
+                  </b-link>
+                </b-list-group-item>
                 <b-list-group-item>
                   <b-link to="/bene/ricerca">
-                  Ricerca un bene</b-link>
-                  </b-list-group-item>
+                    Ricerca un bene
+                  </b-link>
+                </b-list-group-item>
                 <b-list-group-item>
                   <b-link to="/funzione/aggiungi">
-                  Aggiungi una funzione tra due beni</b-link>
+                    Aggiungi una funzione tra due beni
+                  </b-link>
                 </b-list-group-item>
                 <b-list-group-item>
                   <b-link to="/aiuto">Aiuto</b-link>
@@ -67,88 +73,113 @@
               </b-list-group>
             </b-card>
           </b-col>
-          <!-- Statistiche -->
-          <b-col class="text-left mt-1" cols="12" md="6" lg="3">
-            <b-card header="Info e statistiche" no-footer>
-              <p>Il tuo ruolo è
-                <b-badge variant="info">{{$store.getters.getUserData.role}}</b-badge>
-              </p>
-              <p>Gli ID a te riservati variano da
-                <b-badge variant="info">{{data.id_min}}</b-badge>
-                a
-                <b-badge variant="info">{{data.id_max}}</b-badge>
-              </p>
-              <p>Il tuo ultimo ID usato per un Bene è
-                <b-badge variant="info">{{data.ultimo_id_bene}}</b-badge>
-              </p>
-              <p v-if="IDInEsaurimento" class="bg-warning d-inline-block rounded">
-                ATTENZIONE: Stai per esaurire gli ID.
-              </p>
-              <hr/>
-              <div v-if="isRevisore" key="revisorePage">
-                <p>Hai
-                  <b-badge variant="info">{{data.n_beni_da_revisionare}}</b-badge>
-                  <!-- Questo è amore per l'utente, se è 1 scrivo Bene, altrimenti Beni -->
-                  {{data.n_beni_da_revisionare | formatBeni}}
-                  da revisionare.</p>
-                <p>Hai
-                  <b-badge variant="info">{{data.n_funzioni_da_revisionare}}</b-badge>
-                  <!-- Questo è amore per l'utente, se è 1 scrivo Funzione, altrimenti Funzioni -->
-                  {{data.n_funzioni_da_revisionare | formatFunzioni}}
-                  da revisionare.</p>
-              </div>
-              <div v-else key="schedatorePage">
-                <p>Hai
-                  <b-badge variant="info">{{data.n_beni_pronti}}</b-badge>
-                  <!-- Questo è amore per l'utente, se è 1 scrivo Bene, altrimenti Beni -->
-                  {{data.n_beni_pronti | formatBeni}}
-                  in attesa di invio.</p>
-                <p>Hai
-                  <b-badge variant="info">{{data.n_funzioni_pronte}}</b-badge>
-                  <!-- Questo è amore per l'utente, se è 1 scrivo Funzione, altrimenti Funzioni -->
-                  {{data.n_funzioni_pronte | formatFunzioni}}
-                  in attesa di invio.</p>
-                <p>Hai
-                  <b-badge variant="info">{{data.n_beni_rev}}</b-badge>
-                  <!-- Questo è amore per l'utente, se è 1 scrivo Bene, altrimenti Beni -->
-                  {{data.n_beni_rev | formatBeni}}
-                  in attesa di revisione.</p>
-                <p>Hai
-                  <b-badge variant="info">{{data.n_funzioni_rev}}</b-badge>
-                  <!-- Questo è amore per l'utente, se è 1 scrivo Funzione, altrimenti Funzioni -->
-                  {{data.n_funzioni_rev | formatFunzioni}}
-                  in attesa di revisione.</p>
-                <p>Hai
-                  <b-badge variant="info">{{data.n_beni_da_correggere}}</b-badge>
-                  <!-- Questo è amore per l'utente, se è 1 scrivo Bene, altrimenti Beni -->
-                  {{data.n_beni_da_correggere | formatBeni}}
-                  da correggere.</p>
-                <p>Hai
-                  <b-badge variant="info">{{data.n_funzioni_da_correggere}}</b-badge>
-                  <!-- Questo è amore per l'utente, se è 1 scrivo Funzione, altrimenti Funzioni -->
-                  {{data.n_funzioni_da_correggere | formatFunzioni}}
-                  da correggere.</p>
-                <p>Hai
-                  <b-badge variant="info">{{data.n_beni_incompleti}}</b-badge>
-                  <!-- Questo è amore per l'utente, se è 1 scrivo Bene, altrimenti Beni -->
-                  {{data.n_beni_incompleti | formatBeni}}
-                  incompleti.</p>
-                <p>Hai
-                  <b-badge variant="info">{{data.n_funzioni_incomplete}}</b-badge>
-                  <!-- Questo è amore per l'utente, se è 1 scrivo Funzione, altrimenti Funzioni -->
-                  {{data.n_funzioni_incomplete | formatFunzioni}}
-                  incomplete.</p>
-              </div>
-            </b-card>
-          </b-col>
-          <!-- Utenti & logout -->
-          <b-col cols="12">
-            <hr class="w-50"/>
-            <b-button to="/gestisci_utenti" variant="warning"
-              v-if="isRevisore">Aggiungi / Modifica utenti</b-button>
-            <b-button @click="logout">Logout</b-button>
-          </b-col>
-      </b-row>
+        </b-row>
+      </b-col>
+      <!-- Statistiche -->
+      <b-col class="text-left mt-1" cols="auto">
+        <b-card header="Info e statistiche" no-footer>
+          <p>
+            Il tuo ruolo è
+            <b-badge variant="info">{{$store.getters.getUserData.role}}</b-badge>
+          </p>
+          <p>
+            Gli ID a te riservati variano da
+            <b-badge variant="info">{{data.id_min}}</b-badge>
+            a
+            <b-badge variant="info">{{data.id_max}}</b-badge>
+          </p>
+          <p>
+            Il tuo ultimo ID usato per un Bene è
+            <b-badge variant="info">{{data.ultimo_id_bene}}</b-badge>
+          </p>
+          <p v-if="IDInEsaurimento" class="bg-warning d-inline-block rounded">
+            ATTENZIONE: Stai per esaurire gli ID.
+          </p>
+          <hr/>
+          <div v-if="isRevisore" key="revisorePage">
+            <p>
+              Hai
+              <b-badge variant="info">{{data.n_beni_da_revisionare}}</b-badge>
+              <!-- Questo è amore per l'utente, se è 1 scrivo Bene, altrimenti Beni -->
+              {{data.n_beni_da_revisionare | formatBeni}}
+              da revisionare.
+            </p>
+            <p>
+              Hai
+              <b-badge variant="info">{{data.n_funzioni_da_revisionare}}</b-badge>
+              <!-- Questo è amore per l'utente, se è 1 scrivo Funzione, altrimenti Funzioni -->
+              {{data.n_funzioni_da_revisionare | formatFunzioni}}
+              da revisionare.
+            </p>
+          </div>
+          <div v-else key="schedatorePage">
+            <p>
+              Hai
+              <b-badge variant="info">{{data.n_beni_pronti}}</b-badge>
+              <!-- Questo è amore per l'utente, se è 1 scrivo Bene, altrimenti Beni -->
+              {{data.n_beni_pronti | formatBeni}}
+              in attesa di invio.
+            </p>
+            <p>
+              Hai
+              <b-badge variant="info">{{data.n_funzioni_pronte}}</b-badge>
+              <!-- Questo è amore per l'utente, se è 1 scrivo Funzione, altrimenti Funzioni -->
+              {{data.n_funzioni_pronte | formatFunzioni}}
+              in attesa di invio.
+            </p>
+            <p>
+              Hai
+              <b-badge variant="info">{{data.n_beni_rev}}</b-badge>
+              <!-- Questo è amore per l'utente, se è 1 scrivo Bene, altrimenti Beni -->
+              {{data.n_beni_rev | formatBeni}}
+              in attesa di revisione.
+            </p>
+            <p>
+              Hai
+              <b-badge variant="info">{{data.n_funzioni_rev}}</b-badge>
+              <!-- Questo è amore per l'utente, se è 1 scrivo Funzione, altrimenti Funzioni -->
+              {{data.n_funzioni_rev | formatFunzioni}}
+              in attesa di revisione.
+            </p>
+            <p>
+              Hai
+              <b-badge variant="info">{{data.n_beni_da_correggere}}</b-badge>
+              <!-- Questo è amore per l'utente, se è 1 scrivo Bene, altrimenti Beni -->
+              {{data.n_beni_da_correggere | formatBeni}}
+              da correggere.
+            </p>
+            <p>
+              Hai
+              <b-badge variant="info">{{data.n_funzioni_da_correggere}}</b-badge>
+              <!-- Questo è amore per l'utente, se è 1 scrivo Funzione, altrimenti Funzioni -->
+              {{data.n_funzioni_da_correggere | formatFunzioni}}
+              da correggere.
+            </p>
+            <p>
+              Hai
+              <b-badge variant="info">{{data.n_beni_incompleti}}</b-badge>
+              <!-- Questo è amore per l'utente, se è 1 scrivo Bene, altrimenti Beni -->
+              {{data.n_beni_incompleti | formatBeni}}
+              incompleti.
+            </p>
+            <p>
+              Hai
+              <b-badge variant="info">{{data.n_funzioni_incomplete}}</b-badge>
+              <!-- Questo è amore per l'utente, se è 1 scrivo Funzione, altrimenti Funzioni -->
+              {{data.n_funzioni_incomplete | formatFunzioni}}
+              incomplete.
+            </p>
+          </div>
+        </b-card>
+      </b-col>
+      <!-- Utenti & logout -->
+      <b-col cols="12">
+        <hr class="w-50"/>
+        <b-button to="/gestisci_utenti" variant="warning"
+          v-if="isRevisore">Aggiungi / Modifica utenti</b-button>
+        <b-button @click="logout">Logout</b-button>
+      </b-col>
+    </b-row>
   </b-container>
 </template>
 
@@ -218,7 +249,7 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-p:last-child {
+p:last-of-type {
   margin-bottom: 0px;
 }
 .badge {
