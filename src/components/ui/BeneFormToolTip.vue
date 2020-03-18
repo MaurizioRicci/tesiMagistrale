1be4a03d687913b146d9a15e7f9cc60360a7f1d4
mauricci <MaurizioRicci@users.noreply.github.com>
2020-03-18 10:13:59 +0100

    tooltip aggiornato testo

--- src/components/ui/BeneFormToolTip.vue
+++ src/components/ui/BeneFormToolTip.vue
@@ -1,18 +1,40 @@
 <template>
-    <div>
-        <b-tooltip :target="prefix+'checkbox-bozza'" triggers="hover">Specifica se il bene è una bozza o se è pronto per essere inviato (su richiesta dell'utente)</b-tooltip>
-        <b-tooltip :target="prefix+'input-id'" triggers="hover">ID del bene</b-tooltip>
-        <b-tooltip :target="prefix+'input-identificazione'" triggers="hover">Identificazione del bene</b-tooltip>
-        <b-tooltip :target="prefix+'input-descrizione'" triggers="hover">Descrizione del bene</b-tooltip>
-        <b-tooltip :target="prefix+'input-macro-epoca-orig'" triggers="hover">Macro epoca originaria</b-tooltip>
-        <b-tooltip :target="prefix+'input-macro-epoca-car'" triggers="hover">Macro epoca caratteristica</b-tooltip>
-        <b-tooltip :target="prefix+'input-toponimo'" triggers="hover">Toponimo</b-tooltip>
-        <b-tooltip :target="prefix+'input-esistenza'" triggers="hover">Specifica lo stato del bene</b-tooltip>
-        <b-tooltip :target="prefix+'input-comune'" triggers="hover">Comune attuale di appartenenza</b-tooltip>
-        <b-tooltip :target="prefix+'input-bibliografia'" triggers="hover">Bibliografia</b-tooltip>
-        <b-tooltip :target="prefix+'input-note'" triggers="hover">Eventuali note</b-tooltip>
-        <b-tooltip :target="prefix+'input-geometria'" triggers="hover">Geometria del bene. Sono i vertici di un poligono</b-tooltip>
-    </div>
+ <div>
+  <b-tooltip :target="prefix + 'checkbox-bozza'" triggers="hover">
+    Specifica se il bene è una bozza o se è pronto per essere inviato (su
+   richiesta dell'utente)
+   </b-tooltip>
+  <b-tooltip :target="prefix + 'input-id'" triggers="hover">
+    ID del bene
+  </b-tooltip>
+  <b-tooltip :target="prefix + 'input-identificazione'" triggers="hover">
+   Serve a identificare il Bene in modo che ogni schedatore sia in grado di
+   riconoscerlo ed evitare così doppie acquisizioni.
+  </b-tooltip>
+  <b-tooltip :target="prefix + 'input-descrizione'" triggers="hover">
+    Descrizione del bene
+   </b-tooltip>
+  <b-tooltip :target="prefix + 'input-macro-epoca-orig'" triggers="hover">
+    Macro epoca originaria. Indica la presunta epoca di origine del
+   Bene.
+   </b-tooltip>
+  <b-tooltip :target="prefix + 'input-macro-epoca-car'" triggers="hover">
+    Macro epoca caratteristica. Indica la presunta epoca che più delle altre
+   caratterizza il Bene.
+   </b-tooltip>
+  <b-tooltip :target="prefix + 'input-toponimo'" triggers="hover">
+    Toponimo</b-tooltip>
+  <b-tooltip :target="prefix + 'input-esistenza'" triggers="hover">
+    Specifica lo stato del bene</b-tooltip>
+  <b-tooltip :target="prefix + 'input-comune'" triggers="hover">
+    Comune attuale di appartenenza</b-tooltip>
+  <b-tooltip :target="prefix + 'input-bibliografia'" triggers="hover">
+    Bibliografia</b-tooltip>
+  <b-tooltip :target="prefix + 'input-note'" triggers="hover">
+    Eventuali note</b-tooltip>
+  <b-tooltip :target="prefix + 'input-geometria'" triggers="hover">
+    Geometria del bene. Sono i vertici di un poligono</b-tooltip>
+ </div>
 </template>
 
 <script>
--- src/components/ui/FunzioneFormToolTip.vue
+++ src/components/ui/FunzioneFormToolTip.vue
@@ -1,28 +1,47 @@
 <template>
-    <div>
-        <b-tooltip :target="prefix+'checkbox-bozza'" triggers="hover">Specifica se il bene è una bozza o se è pronto per essere inviato (su richiesta dell'utente)</b-tooltip>
-        <b-tooltip :target="prefix+'input-id_bene'" triggers="hover">ID del bene</b-tooltip>
-        <b-tooltip :target="prefix+'input-id_bener'" triggers="hover">ID del bene di riferimento</b-tooltip>
-        <b-tooltip :target="prefix+'input-denominazione'" triggers="hover">Identificazione del bene</b-tooltip>
-        <b-tooltip :target="prefix+'input-denominazioner'" triggers="hover">Identificazione del bene di riferimento</b-tooltip>
-        <b-tooltip :target="prefix+'input-data'" triggers="hover">Data della funzione</b-tooltip>
-        <b-tooltip :target="prefix+'input-data_ante'" triggers="hover">Primo estremo della data della funzione</b-tooltip>
-        <b-tooltip :target="prefix+'input-data_poste'" triggers="hover">Secondo estremo della data della funzione</b-tooltip>
-        <b-tooltip :target="prefix+'input-tipodata'" triggers="hover">
-          <span>Tipo data della funzione può assumere i seguenti valori.</span>
-          <ul class="text-left">
-            <li>niente prima: la relazione vale dalla data specificata in poi</li>
-            <li>niente dopo: la relazione è valida fino ala data specificata</li>
-            <li>niente prima e dopo: la relazione vale solo in quel preciso periodo</li>
-          </ul>
-          <span>Se il campo viene lasciato vuoto si assume che non si conosca nulla circa la validità delle informazioni fuori dall’intervallo temporale specificato.</span>
-        </b-tooltip>
-        <b-tooltip :target="prefix+'input-funzione'" triggers="hover">Funzione che lega due beni</b-tooltip>
-        <b-tooltip :target="prefix+'input-bibliografia'" triggers="hover">Bibliografia</b-tooltip>
-        <b-tooltip :target="prefix+'input-note'" triggers="hover">Eventuali note</b-tooltip>
-        <b-tooltip :target="prefix+'input-ruolo'" triggers="hover">Ruolo del bene nella funzione/relazione</b-tooltip>
-        <b-tooltip :target="prefix+'input-ruolor'" triggers="hover">Ruolo del bene di riferimento nella funzione/relazione</b-tooltip>
-    </div>
+ <div>
+  <b-tooltip :target="prefix + 'checkbox-bozza'" triggers="hover"
+   >Specifica se il bene è una bozza o se è pronto per essere inviato (su
+   richiesta dell'utente)</b-tooltip>
+  <b-tooltip :target="prefix + 'input-id_bene'" triggers="hover"
+   >ID del bene</b-tooltip>
+  <b-tooltip :target="prefix + 'input-id_bener'" triggers="hover"
+   >ID del bene di riferimento</b-tooltip>
+  <b-tooltip :target="prefix + 'input-denominazione'" triggers="hover"
+   >Identificazione del bene. Riporta il nome con cui il Bene è citato nel
+   documento.</b-tooltip>
+  <b-tooltip :target="prefix + 'input-denominazioner'" triggers="hover"
+   >Identificazione del bene di riferimento. Riporta il nome con cui il Bene di
+   riferimento è citato nel documento.</b-tooltip>
+  <b-tooltip :target="prefix + 'input-data'" triggers="hover"
+   >Data della funzione</b-tooltip>
+  <b-tooltip :target="prefix + 'input-data_ante'" triggers="hover"
+   >Primo estremo della data della funzione</b-tooltip>
+  <b-tooltip :target="prefix + 'input-data_poste'" triggers="hover"
+   >Secondo estremo della data della funzione</b-tooltip>
+  <b-tooltip :target="prefix + 'input-tipodata'" triggers="hover">
+   <span>Tipo data della funzione può assumere i seguenti valori.</span>
+   <ul class="text-left">
+    <li>niente prima: la relazione vale dalla data specificata in poi</li>
+    <li>niente dopo: la relazione è valida fino ala data specificata</li>
+    <li>niente prima e dopo: la relazione vale solo in quel preciso periodo</li>
+   </ul>
+   <span
+    >Se il campo viene lasciato vuoto si assume che non si conosca nulla circa
+    la validità delle informazioni fuori dall’intervallo temporale
+    specificato.</span>
+  </b-tooltip>
+  <b-tooltip :target="prefix + 'input-funzione'" triggers="hover"
+   >Funzione che lega due beni</b-tooltip>
+  <b-tooltip :target="prefix + 'input-bibliografia'" triggers="hover"
+   >Bibliografia</b-tooltip>
+  <b-tooltip :target="prefix + 'input-note'" triggers="hover"
+   >Eventuali note</b-tooltip>
+  <b-tooltip :target="prefix + 'input-ruolo'" triggers="hover"
+   >Ruolo del bene nella funzione/relazione</b-tooltip>
+  <b-tooltip :target="prefix + 'input-ruolor'" triggers="hover"
+   >Ruolo del bene di riferimento nella funzione/relazione</b-tooltip>
+ </div>
 </template>
 
 <script>
